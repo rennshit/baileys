09c27bd4f437cd2cc48fdcb9e71e4fb60ebb70d3
rennshit <venzyyy0@gmail.com>
2026-09-08 01:12:57 +0700

    sync Vansnowi pairing connection

--- lib/Defaults/index.d.ts
+++ lib/Defaults/index.d.ts
@@ -9,7 +9,7 @@ export declare const DEF_TAG_PREFIX = "TAG:";
 export declare const PHONE_CONNECTION_CB = "CB:Pong";
 export declare const WA_DEFAULT_EPHEMERAL: number;
 export declare const NOISE_MODE = "Noise_XX_25519_AESGCM_SHA256\0\0\0\0";
-export declare const DICT_VERSION = 2;
+export declare const DICT_VERSION = 3;
 export declare const KEY_BUNDLE_TYPE: Buffer;
 export declare const NOISE_WA_HEADER: Buffer;
 /** from: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url */
